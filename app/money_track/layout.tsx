5ba656b3d6df3b0042f832cfd2fe2b7e497a094a
Rain045 <2497005420@qq.com>
2026-01-27 17:28:40 +0800

    Fixed Code

---
+++ app/money_track/layout.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main>
+        {/* 左上角导航区 */}
+        <nav className="mb-0 pl-4 pt-4 md:pl-4 md:pt-4">
+            <Link 
+                href="/" 
+                className={`inline-flex items-center text-xl text-white hover:text-blue-800 transition-colors font-medium ${lusitana.className}`}
+            >
+                <span className="mr-2">&lt;&lt;</span>
+                Home
+            </Link>
+        </nav>
+
+        {/* 子页面内容区 */}
+        <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </main>
+    
+  );
+}
