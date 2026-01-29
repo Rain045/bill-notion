6f8dbbe1174537ef6668fa1178a5775b9ae5364b
Rain045 <2497005420@qq.com>
2026-01-29 09:11:37 +0800

    Set PWA UserScalable:false

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import type { Metadata } from "next";
 import "@/app/ui/globals.css";
 import { lusitana } from "@/app/ui/fonts";
+import type { Viewport } from 'next';
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false, // 禁止用户缩放
+  // 针对 iOS PWA 的额外设置
+  themeColor: '#ffffff',
+};
 
 export const metadata: Metadata = {
   title: "Bill Notion",
